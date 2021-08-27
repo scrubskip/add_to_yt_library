2ce6e82ff4d1f8f20c41611b79b8e03a2f22d3f1
Justin Koh <kip@kipster.org>
2021-08-26 23:51:37 -0700

    Mostly works, except for soundtracks.

--- add_to_library.py
+++ add_to_library.py
@@ -6,19 +6,24 @@ yt = YTMusic('headers_auth.json')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", type=str)
-parser.add_argument("dry_run", type=bool)
+parser.add_argument('--dry-run', dest='dry_run', action='store_true')
+parser.add_argument('--no-dry-run', dest='dry_run', action='store_false')
 args = parser.parse_args()
 
 def main():
-
     print(args.input)
+    if (args.dry_run):
+        playlistId = "test"
+    else:
+        playlistId = yt.create_playlist("Import 3", "imported song")
+
     with open(args.input) as albums:
         album_reader = csv.reader(albums, delimiter='\t')
         errors = []
         success = []
         for album in album_reader:
             print(album)
-            if search_and_add(album):
+            if search_and_add(album, playlistId):
                 success.append(album)
             else:
                 errors.append(album)
@@ -26,7 +31,7 @@ def main():
         print("Errors:")
         print(*errors)
 
-def search_and_add(album):
+def search_and_add(album, playlistId = None):
     results = yt.search(album[0] + " " + album[1], filter = "albums")
     added = False
     if results is not None:
@@ -35,7 +40,11 @@ def search_and_add(album):
             ## break if this is really similar otherwise move to the next one.
             if (result_title.find(str(album[0]).lower()) != -1 or result_title.find(str(album[1].lower())) != -1):
                 print(result['title'], " id: ", result['browseId'])
-                add_album(result['browseId'])
+                trackIds = add_album(result['browseId'])
+                if (not args.dry_run):
+                    response = yt.add_playlist_items(playlistId, trackIds)
+                else:
+                    print("would have added ", len(trackIds), " to ", playlistId)
                 added = True
                 break
     return added
@@ -43,14 +52,21 @@ def search_and_add(album):
 
 def add_album(albumId):
     album = yt.get_album(albumId)
+    trackIds = []
     if (album is not None):
         ## loop through the tracks, add to library
         for track in album['tracks']:
             if not args.dry_run:
-                response = yt.edit_song_library_status(track['feedbackTokens']['add'])
-                if (not response['feedbackResponses'][0]['isProcessed']):
-                    print("Error adding ", track['title'], " from ", album['title'])
+                if 'feedbackTokens' in track and track['feedbackTokens']['add']:
+                    response = yt.edit_song_library_status(track['feedbackTokens']['add'])
+                    if (not response['feedbackResponses'][0]['isProcessed']):
+                        print("Error adding ", track['title'], " from ", album['title'])
             else:
-                print("would have added ", track['title'])
+                if 'feedbackToken' in track:
+                    print("already have ", track['title'])
+                elif 'feedbackTokens' in track:
+                    print("would have added ", track['title'])
+            trackIds.append(track['videoId'])
+    return trackIds
 
 main()
